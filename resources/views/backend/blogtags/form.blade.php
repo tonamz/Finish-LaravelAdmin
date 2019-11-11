cd2d889034928dcd1fa2896813316e4af4053799
tonamz <dtcstudiointh@gmail.com>
2019-11-11 14:50:15 +0700

    all
    update composer
    
    
    finish!!!!!!!!
    
    
    Module style
    
    
    style admin
    
    
    mudule style
    
    
    Banner
    
    
    Revert "Banner"
    
    This reverts commit 7d0f95d0cb7e8c1b52f202746a61335ff26c7606.
    
    module style
    
    
    db with docker data

---
+++ resources/views/backend/blogtags/form.blade.php
@@ -0,0 +1,24 @@
+<div class="box-body">
+    <div class="form-group">
+        {{ Form::label('name', trans('validation.attributes.backend.blogtags.title'), ['class' => 'col-lg-2 control-label required']) }}
+
+        <div class="col-lg-12">
+            {{ Form::text('name', null, ['class' => 'form-control box-size', 'placeholder' => trans('validation.attributes.backend.blogtags.title'), 'required' => 'required']) }}
+        </div><!--col-lg-10-->
+    </div><!--form control-->
+
+    <div class="form-group">
+
+        <div class="col-lg-12">
+            <div class="control-group">
+                <div class="checkbox checkbox-info mb-2">
+                        <input  checked="checked" id="checkbox4" type="checkbox" id="status" name="status" value="1">
+                        <label for="checkbox4">
+                            Active
+                        </label>
+                </div> 
+            <div class="control__indicator"></div>
+                </label>
+        </div><!--col-lg-3-->
+    </div><!--form control-->
+</div>
---
+++ vendor/bvipul/generator/src/views/partials/modules-header-buttons.blade.php
@@ -0,0 +1,29 @@
+<!--Action Button-->
+    @if(Active::checkUriPattern('admin/modules'))
+        <div class="btn-group">
+          <button type="button" class="btn btn-warning btn-flat dropdown-toggle" data-toggle="dropdown">Export
+            <span class="fas fa-caret-down"></span>
+            <span class="sr-only">Toggle Dropdown</span>
+          </button>
+          <ul class="dropdown-menu dropdown-menu-right" role="menu">
+            <li id="copyButton"><a href="#"><i class="fa fa-clone"></i> Copy</a></li>
+            <li id="csvButton"><a href="#"><i class="far fa-file-alt"></i> CSV</a></li>
+            <li id="excelButton"><a href="#"><i class="fa fa-file-excel"></i> Excel</a></li>
+            <li id="pdfButton"><a href="#"><i class="fa fa-file-pdf"></i> PDF</a></li>
+            <li id="printButton"><a href="#"><i class="fa fa-print"></i> Print</a></li>
+          </ul>
+        </div>
+    @endif
+<!--Action Button-->
+<div class="btn-group">
+  <button type="button" class="btn btn-blue btn-flat dropdown-toggle" data-toggle="dropdown">Action
+    <span class="fas fa-caret-down"></span>
+    <span class="sr-only">Toggle Dropdown</span>
+  </button>
+  <ul class="dropdown-menu dropdown-menu-right" role="menu">
+    <li><a href="{{route('admin.modules.index')}}"><i class="fa fa-list-ul"></i> {{trans('menus.backend.modules.all')}}</a></li>
+    <li><a href="{{route('admin.modules.create')}}"><i class="fa fa-plus"></i> {{trans('menus.backend.modules.create')}}</a></li>
+  </ul>
+</div>
+
+<div class="clearfix"></div>
